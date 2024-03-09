cb0b331e6b5eadc19c4a593d13f98f8b04fbfca1
Adham Salama <adham.salama@wuilt.com>
2024-03-09 21:13:18 +0200

    Add types

--- server.ts
+++ server.ts
@@ -17,23 +17,34 @@ const app = http
   })
   .listen(8080);
 console.log("Listening on http://localhost:8080");
+type Message =
+  | RTCSessionDescriptionInit
+  | RTCIceCandidateInit
+  | CandidateMessage
+  | "peerIsReady"
+  | "bye";
+type CandidateMessage = {
+  type: "candidate";
+  label: number;
+  id: string;
+  candidate: string;
+};
 const io = new Server(app);
-
 io.sockets.on("connection", function (socket: any) {
   // convenience function to console.log server messages on the client
-  socket.on("message", function (message: any) {
+  socket.on("message", function (message: Message) {
     console.log("Client said: ", message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit("message", message);
   });
 
-  socket.on("createRoom", (room: any) => {
+  socket.on("createRoom", (room: string) => {
     socket.join(room);
     console.log("Client ID " + socket.id + " created room " + room);
     socket.emit("created", room, socket.id);
   });
 
-  socket.on("joinRoom", (room: any) => {
+  socket.on("joinRoom", (room: string) => {
     console.log("Client ID " + socket.id + " joined room " + room);
     io.sockets.in(room).emit("join", room);
     socket.join(room);
