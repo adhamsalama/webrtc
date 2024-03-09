daec7e6426fda8c73b15172c410cb9e824cb00e6
Adham Salama <adham.salama@wuilt.com>
2024-03-09 21:02:04 +0200

    Modernize the server

--- index.ts
+++ index.ts
@@ -1,58 +1,44 @@
-const http = require("http");
-const fs = require("fs/promises");
-const socketIO = require("socket.io");
-const os = require("os");
+import http from "node:http";
+import fs from "node:fs/promises";
+import { Server } from "socket.io";
+
 const app = http
   .createServer(async (req, res) => {
-    if (req.url?.endsWith(".html")) {
+    if (req.url?.endsWith("/index.html")) {
       const file = await fs.readFile("./index.html");
       return res.end(file);
     }
-    if (req.url?.endsWith(".js")) {
+    if (req.url?.endsWith("/js/main.js")) {
       const file = await fs.readFile("./js/main.js");
       return res.end(file);
     }
-    if (req.url?.endsWith(".mp4")) {
-      const file = await fs.readFile("./sample-5s.mp4");
-      return res.setHeader("Content-Type", "video/mp4").end(file);
-    }
+
     return res.end();
   })
   .listen(8080);
 console.log("Listening on http://localhost:8080");
-const io = socketIO.listen(app);
+const io = new Server(app);
 
-io.sockets.on("connection", function (socket) {
+io.sockets.on("connection", function (socket: any) {
   // convenience function to console.log server messages on the client
-  socket.on("message", function (message) {
+  socket.on("message", function (message: any) {
     console.log("Client said: ", message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit("message", message);
   });
 
-  socket.on("createRoom", (room) => {
+  socket.on("createRoom", (room: any) => {
     socket.join(room);
     console.log("Client ID " + socket.id + " created room " + room);
     socket.emit("created", room, socket.id);
   });
 
-  socket.on("joinRoom", (room) => {
+  socket.on("joinRoom", (room: any) => {
     console.log("Client ID " + socket.id + " joined room " + room);
     io.sockets.in(room).emit("join", room);
     socket.join(room);
     socket.emit("joined", room, socket.id);
     io.sockets.in(room).emit("ready");
-  });
-
-  socket.on("ipaddr", function () {
-    const ifaces = os.networkInterfaces();
-    for (const dev in ifaces) {
-      ifaces[dev]?.forEach(function (details) {
-        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
-          socket.emit("ipaddr", details.address);
-        }
-      });
-    }
   });
 
   socket.on("bye", function () {
